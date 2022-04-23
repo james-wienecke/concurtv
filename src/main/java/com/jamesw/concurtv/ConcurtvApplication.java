613f23cff83969d4e2e32fbf128e19d8fad052a7
James Wienecke <james.a.wienecke@gmail.com>
2022-04-22 22:24:15 -0500

    Basic project setup.

---
+++ src/main/java/com/jamesw/concurtv/ConcurtvApplication.java
@@ -0,0 +1,13 @@
+package com.jamesw.concurtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurtvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesw/concurtv/ConcurtvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesw.concurtv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
