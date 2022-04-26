85a1c66be8180fd521c3ed7bcf8e7825454fd148
James Wienecke <james.a.wienecke@gmail.com>
2022-04-25 19:27:59 -0500

    Created basic jpa repos for existing models.

---
+++ src/main/java/com/jamesw/concurtv/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.jamesw.concurtv.repository;
+
+import com.jamesw.concurtv.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
