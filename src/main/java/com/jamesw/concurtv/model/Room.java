bbb34312f5f2b9778bfd5d5cd07beaf149e35a07
James Wienecke <james.a.wienecke@gmail.com>
2022-04-27 22:50:32 -0500

    Extremely basic non-relational columns created for room model.

--- src/main/java/com/jamesw/concurtv/model/Room.java
+++ src/main/java/com/jamesw/concurtv/model/Room.java
@@ -14,5 +14,42 @@ public class Room {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "")
+    @Column(name = "description")
+    private String description;
+
+//    RELATIONS TBD LATER
+
+
+    public Room() {
+    }
+
+    public Room(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
